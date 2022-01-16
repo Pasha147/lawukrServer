1e56cd808bf929672c19018dd4cf0d59b1ac00b2
Pasha <75729255+Pasha147@users.noreply.github.com>
2022-01-16 15:24:49 +0200

    Messages are stored in the db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,18 +15,36 @@ app.use((req, res, next) => {
   next();
 });
 
+//=======Mongoose=====================
+const schema = new Schema({
+  message: { type: String, required: true },
+});
+const Mod = model("message", schema);
+
+async function saveToDb(message) {
+  const mod = new Mod({ message });
+  await mod.save();
+}
+//=================================
+
 //POST
 app.post("/api/data/:id", (req, res) => {
   console.log("req.params>>", req.params);
   console.log("req.body>>", req.body);
-  res.status(200).json("The message has been reseived");
+  saveToDb(JSON.stringify(req.body));
+
+  //   let reqBodyStr = JSON.stringify(req.body);
+  //   console.log(reqBodyStr);
+  //   console.log(JSON.parse(reqBodyStr));
+
+  res.status(200).json(`The message from ${req.body.name} has been reseived`);
 });
 
 async function start() {
   try {
     url =
       "mongodb+srv://Pasha147:P1471472@cluster0.ytmzn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-    // await mongoose.connect(url, {});
+    await mongoose.connect(url, {});
     app.listen(PORT, () => {
       console.log("Server has been started at PORT ", PORT);
     });
